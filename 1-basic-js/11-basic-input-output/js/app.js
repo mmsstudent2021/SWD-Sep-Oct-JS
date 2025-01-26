ac01120f510c16227f2ade2c9a394716a3608417
mmsstudent2021 <mmsstudent2021@gmail.com>
2025-01-26 12:50:17 +0630

    filter,find,findIndex,reduce,window

---
+++ 1-basic-js/11-basic-input-output/js/app.js
@@ -0,0 +1,33 @@
+// console 
+// window 
+// dom 
+
+// console.log(console)
+// // console.log("hello");
+// // console.clear()
+
+// console.log(window);
+
+// console.log(document.getElementsByTagName("h1"));
+
+// 1 + 2 
+// const x = 1
+// const y = 2
+// const result = x + y
+// console.log(result);
+
+// input 
+const x = window.prompt("input first number")
+const y = window.prompt("input second number")
+console.log(typeof parseFloat(x));
+console.log(typeof parseFloat(x));
+console.log(parseFloat(x) + parseFloat(y));
+
+// // output 
+window.alert(parseFloat(x) + parseFloat(y))
+
+// confirm => return boolean 
+const result = window.confirm("ser pp lar")
+console.log(`${result ? "ser pp" : "ma ser ya ty buu"} `);
+
+// birth year => 2002
